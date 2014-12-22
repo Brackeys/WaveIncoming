8a52f0821e8f7dcfa766cea71b0926fa23f18795
Asbjørn Thirslund <at@brackeys.com>
2014-12-22 17:29:50 +0100

    Lay foundation for quality settings menu

---
+++ Assets/Scripts/QualitySettingsManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QualitySettingsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Resolution[] resolutions = Screen.resolutions;
+		foreach (Resolution res in resolutions) {
+			print(res.width + "x" + res.height);
+		}
+		Screen.SetResolution(resolutions[0].width, resolutions[0].height, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
